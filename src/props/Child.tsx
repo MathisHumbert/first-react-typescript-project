e7e397ebbfaf607007d77b3be34d0e980ecd0142
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-01 16:44:32 +0100

    working on rts

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,3 +1,22 @@
-export const Child = () => {
-  return <div>Hi there!</div>;
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+}
+
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
